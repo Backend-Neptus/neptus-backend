d269e3bcc4355ce29b7f773381c2d18d83794feb
Elyza Braga Duarte de Oliveira <70656981+elyza-braga@users.noreply.github.com>
2025-06-18 22:32:56 -0300

    sensores
    Adicionada sensor_controller e sensor_service.
    Modifiquei o sensor_model para ter os componentes pedidos (usuario_id, propriedade_id) e coloquei estes no turbidez_model para testar.

--- app/models/sensor_model.py
+++ app/models/sensor_model.py
@@ -1,9 +1,28 @@
 from app import db
-
+from sqlalchemy.dialects.postgresql import UUID
+from datetime import datetime
+import uuid
 
 class Sensor(db.Model):
+  __tablename__ = 'sensor'
   id = db.Column(db.Integer, primary_key=True)
   nome = db.Column(db.String, nullable=False)
+  turbidez_id = db.Column(UUID(as_uuid=True), db.ForeignKey('turbo.id'), nullable=False, unique=True)
+  created_at = db.Column(db.DateTime,default=datetime.utcnow)
+  updated_at = db.Column(db.DateTime,
+                         default=datetime.utcnow)
+
 
   def __repr__(self):
     return f"<Sensor {self.id}, {self.nome}>"
+  
+  def to_dict(self):
+    return {
+        'id': self.id,
+        'nome': self.nome,
+        'turbidez_id': str(self.turbidez_id),
+        'usuario_id': str(self.turbidez.usuario_id),
+        'proprietario_id': str(self.turbidez.proprietario_id),
+        'created_at': self.created_at.strftime('%d/%m/%Y %H:%M:%S'),
+        'updated_at': self.updated_at.strftime('%d/%m/%Y %H:%M:%S')
+    }
--- app/models/turbidez_model.py
+++ app/models/turbidez_model.py
@@ -1,8 +1,14 @@
 from app import db
+from sqlalchemy.dialects.postgresql import UUID
 
 class Turbidez(db.Model):
+    __tablename__ = 'turbidez'
     id = db.Column(db.Integer, primary_key=True)
     valor = db.Column(db.String, nullable=False)
+    proprietario_id = db.Column(UUID(as_uuid=True), db.ForeignKey('usuario.id'))
+    usuario_id = db.Column(UUID(as_uuid=True),
+                        db.ForeignKey('usuario.id'),
+                        nullable=False)
 
     def __repr__(self):
         return f"<Turbidez {self.id}, {self.valor}>"
